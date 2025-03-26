442417c93f9500e314581645a1c4ac6748ad33dd
hafsahussaini <50855513+hafsahussaini@users.noreply.github.com>
2025-03-26 15:27:44 +0000

    updated to Number

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -7,9 +7,9 @@
 int main(){
     float num_1, num_2, result;
 
-    std::cout << "Enter number 1: " << std::flush;
+    std::cout << "Enter Number 1: " << std::flush;
     std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
+    std::cout << "Enter Number 2: " << std::flush;
     std::cin >> num_2;
     
     result = num_1 + num_2;
